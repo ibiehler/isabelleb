f29c0c403715f0c0c9127009cac700d56797ce46
isabelle <ibiehler@fwparker.org>
2020-02-06 13:37:20 -0600

    Added imports notes

--- Labs/hangman.py
+++ Labs/hangman.py
@@ -1,22 +1,14 @@
 # Hangman game
-
+# NOTE: I already made a hangman game over the summer (I showed you this) so I edited it to fit with this assignment
+# I can show it to you again if you'd like
+import random
 # PSEUDOCODE
 # setup your game by doing the following
-# make a word list for your game
-# grab a random word from your list and store it as a variable
-
-# in a loop, do the following
-# display the hangman using the gallows
-# display the used letters so the user knows what has been selected
-# display the length of the word to the user using blank spaces and used letters
-# prompt the user to guess a letter
-# don't allow the user to select the same letter twice
-# if the guess is incorrect increment incorrect_guesses by 1
-# if the incorrect_guesses is greater than 8, tell the user they lost and exit the program
-# if the user gets all the correct letters, tell the user they won
-
-# ask if they want to play again
-
+mylistofwords = ["discombobulated", "sophistication", "emboss", "canoodle", "skedaddle", "gobbledygook", "materialize",
+                 "imperatively", "myriad", "sentimentality", "physiological", "cliche", "fisticuffs", "dimwit"]
+chosen = random.choice(mylistofwords)
+wordlength = len(chosen)
+done = False
 gallows = [
     '''
       +---+
@@ -82,5 +74,88 @@ gallows = [
     =========
     '''
     ]
+# in a loop, do the following:
+# display the hangman using the gallows *
+# display the used letters so the user knows what has been selected *
+# display the length of the word to the user using blank spaces and used letters *
+# prompt the user to guess a letter *
+# don't allow the user to select the same letter twice *
+# if the guess is incorrect increment incorrect_guesses by 1 *
+# if the incorrect_guesses is greater than 8, tell the user they lost and exit the program *
+# if the user gets all the correct letters, tell the user they won *
+# ask if they want to play again *
 
-print(gallows[4])
+
+def get_current_word(choice, guessed):
+    word = ""
+
+    for letter in choice:
+        if letter in guessed:
+            word += letter
+        else:
+            word += " _ "
+    return word
+
+
+def play_again_func():
+    answer = input("Would you like to play again? ")
+    if answer.lower() == "yes":
+        return answer
+
+
+while not done:
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+        incorrect_guesses = 0
+        gallow_number = 0
+        print("Your word is", wordlength, "letters long.")
+        guessedletters = []
+
+        while incorrect_guesses < 6:  # in the directions you wrote 8, but there were 6 graphics
+            print("\nThe letters you have used: ", guessedletters)
+            print(gallows[gallow_number])
+            player_guess = input("\nGuess a letter of the alphabet: ").lower()
+
+            if player_guess not in alphabet:
+                print("That is not a letter of the alphabet.\n")
+
+            elif player_guess in guessedletters:
+                print("You have already guessed that letter.\n")
+
+            elif player_guess not in chosen:
+                print("\nThat is incorrect!")
+                incorrect_guesses += 1
+                guessedletters.append(player_guess)
+                word = get_current_word(chosen, guessedletters)
+                print(word)
+                gallow_number += 1
+
+            else:
+                guessedletters.append(player_guess)
+                if player_guess in chosen:
+                    print("You guessed correctly!\n")
+                    word = get_current_word(chosen, guessedletters)
+                    print(word)
+
+                    if ' _ ' not in word:
+                        print("\nYou won!")
+                        done = True
+                        del mylistofwords[mylistofwords.index(chosen)]
+                        break
+
+        if incorrect_guesses == 6:
+            print(gallows[6])
+            print("You have taken too many guesses to guess your word; you lose! The word was", chosen)
+            done = True
+
+        if mylistofwords == []:
+            print("You have used up all the words!")
+            break
+
+        if done:
+            if play_again_func():
+                done = False
+                guessedletters = []
+                chosen = random.choice(mylistofwords)
+                wordlength = len(chosen)
+
+
--- Problems/02_lists.py
+++ Problems/02_lists.py
@@ -4,27 +4,68 @@
 # PROBLEM 1 (Using List Comprehensions - 8pts)
 # Use list comprehensions to do the following:
 # a) Make a list of numbers from 1 to 100
+my_list1 = [x for x in range(101)]
+
 # b) Make a list of even numbers from 20 to 40
+my_list2 = [x for x in range(20, 41) if (x ** 2) % 2 == 0]
+
 # c) Make a list of squares from 1 to 100 (1 ** 2 to 100 ** 2)
+my_list3 = [x ** 2 for x in range(101)]
+
 # d) Make a list of all positive numbers in my_list below.
 my_list = [-77, -78, 82, 81, -40, 2, 62, 65, 74, 48, -37, -52, 90, -84, -79, -45, 47, 60, 35, -18]
+my_new_list = [x for x in my_list if x > 0]
+
 
 # PROBLEM 2 (Import the number list - 3pts)
 # The Problems directory contains a file called "number_list.py"
 # import this file which contains num_list
+from Problems import number_list
+
 # Print the last 5 numbers in num_list
+number_list1 = number_list.num_list
+print(number_list1[-5:])
 
 
 # PROBLEM 3 (List functions and methods - 8pts)
 # Find and print the highest number in num_list (1pt)
+print(max(number_list1))
+
 # Find and print the lowest number in num_list (1pt)
+print(min(number_list1))
+
 # Find and print the average of num_list (2pts)
+sum1 = 0
+for i in range(len(number_list1)):
+    sum1 += number_list1[i]
+
+average = sum1/(len(number_list1))
+print(average)
+
 # Remove the lowest number from num_list (2pt)
+del number_list1[number_list1.index(min(number_list1))]
+
 # Create and print a new list called top_ten which contains only the 10 highest numbers in num_list(2pts)
+top_ten = []
+number_list1.sort()
+top_ten.append(number_list1[-10:])
+print(top_ten)
 
 
 # PROBLEM 4 (4pts)
 # Find the number which appears most often in num_list?
+list_being_used = number_list.num_list
+number1 = list_being_used[0]
+prev_frequency = 0
+
+for num in list_being_used:
+    current_frequency = list_being_used.count(num)
+    if current_frequency > prev_frequency:
+        number1 = num
+        prev_frequency = current_frequency
+
+
+print(number1)
 
 
 # CHALLENGE PROBLEMS (2pts)
@@ -32,10 +73,32 @@ my_list = [-77, -78, 82, 81, -40, 2, 62, 65, 74, 48, -37, -52, 90, -84, -79, -45
 
 # Find the number of prime numbers in num_list?
 # Hint: One way is to just start removing the ones that aren't
+number_list2 = number_list.num_list
+prime_number_list = []
+
+# I've made a function like this before (when I did some coding during the summer) so I edited it to match...
+# this question & I can show you the original one if you want
+for num in range(len(number_list2)):
+    number = number_list2[num]
+    for i in range(2, number):
+        if number % i == 0:
+            break
+    else:
+        if number not in prime_number_list:
+            prime_number_list.append(number)
+
+print(len(prime_number_list))
+
 
 # Find the number of palindromes
 # Hint: This may be easier to do with strings
+palindromes = 0
 
+for i in range(len(number_list1)):
+    possible_palindrome = str(number_list1[i])
+    if possible_palindrome[0] == possible_palindrome[3] and possible_palindrome[1] == possible_palindrome[2]:
+        palindromes += 1
 
+print(palindromes)
 
 
