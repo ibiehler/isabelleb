9b94f5ebca456baf39b75ea4cddb93b208466414
isabelle <ibiehler@fwparker.org>
2020-02-24 16:03:26 -0600

    Merge branch 'master' of https://www.github.com/fwparkercode/P2_SP20
    # Conflicts:
    #	Notes/resources/super_villains.txt

--- Labs/Alice Spellcheck/ch15Lab.py
+++ Labs/Alice Spellcheck/ch15Lab.py
@@ -15,6 +15,72 @@ def split_line(line):
     return re.findall('[A-Za-z]+(?:\'[A-Za-z]+)?', line)
 
 
+# Read the file dictionary.txt into an array. Call it dictionary_list or something similar. Then, close the file.
+with open('../Alice Spellcheck/dictionary.txt') as f:
+    dictionary_list = [x.strip().upper() for x in f]
+
+f.close()
+
+# LINEAR SEARCH
+# Print --- Linear Search --- & Open the file AliceInWonderLand200.txt
+print("--- Linear Search ---")
+alice_200 = open('../Alice Spellcheck/AliceInWonderLand200.txt')
+line_number = 0
+
+
+# function
+def linear_search(key, my_list, line_number):
+    i = 0
+    while i < (len(my_list) - 1) and key.upper() != my_list[i]:
+        i += 1
+    if i < (len(my_list) - 1):
+        return True
+    else:
+        print(key, "not found in dictionary. Found in Alice in Wonderland at line", line_number)
+        return False
+
+
+# for loop
+for line in alice_200:
+    line = line.strip().upper()
+    words = split_line(line)
+    line_number += 1
+    for word in words:
+        linear_search(word, dictionary_list, line_number)
+
+
+# BINARY SEARCH
+print("\n--- Binary Search ---")
+file = open('../Alice Spellcheck/AliceInWonderLand200.txt')
+
+# function
+line_number2 = 0
+
+
+def binary_search(key, my_list, line_number):
+    found = False
+    lower_bound = 0
+    upper_bound = len(my_list) - 1
+    while lower_bound <= upper_bound and not found:
+        middle_pos = (upper_bound + lower_bound) // 2
+        if my_list[middle_pos] < key:
+            lower_bound = middle_pos + 1
+        elif my_list[middle_pos] > key:
+            upper_bound = middle_pos - 1
+        else:
+            found = True
+    if found:
+        return True
+    else:
+        print(key, "was not found. Found in Alice in Wonderland at line", line_number)
+
+
+for line in file:
+    line = line.strip().upper()
+    words = split_line(line)
+    line_number2 += 1
+    for word in words:
+        binary_search(word, dictionary_list, line_number2)
 
 
 # Challenge:  Find all words that occur in Alice through the looking glass that do NOT occur in Wonderland.
