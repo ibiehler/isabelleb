b3e650fea568a4cac3161825a8fc288885be7d33
isabelle <ibiehler@fwparker.org>
2020-03-05 13:40:34 -0600

    Merge branch 'master' of https://www.github.com/fwparkercode/P2_SP20
    # Conflicts:
    #	Notes/resources/super_villains.txt

--- Problems/06_gravity_calc.py
+++ Problems/06_gravity_calc.py
@@ -12,9 +12,25 @@
 # (3pts) takes the inputs for mass 1, mass 2, and distance between the two objects (m1, m2, and r)
 # (4pts) contains exceptions for any potential errors (value and dividebyzero).
 # (2pts) keeps asking for inputs until they are valid (see while loop from notes)
-# (3pts) calculates the force of gravity in Newtons and print the result to the user in scientific notation to two decimals.
+# (3pts) calculates the force of gravity in Newtons and print the result to the user in scientific notation to two
+# decimals.
 
+done = False
+G = 6.67e-11
 
+while not done:
+    try:
+        mass1 = int(input("Enter the mass of first object in kg: "))
+        mass2 = int(input("Enter the mass of second object in kg: "))
+        r = int(input("Enter the center to center distance between the objects in meters: "))
+        force = G * (mass1 * mass2) / r**2
+        print(("{:.2e}".format(force)), "is the force of gravity in Newtons")
+        done = True
+    except ValueError as e:
+        print("Invalid conversion")
+        print(e)
+    except ZeroDivisionError as e:
+        print("Error:", e)
 
 
 
