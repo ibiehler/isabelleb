123f0304d1f42a6a4c198996dd5245b804877326
isabelle <ibiehler@fwparker.org>
2020-03-24 21:22:12 -0500

    Merge branch 'master' of https://www.github.com/fwparkercode/P2_SP20
    # Conflicts:
    #	Notes/NotesB/08_recursion.py
    #	Notes/NotesB/09_turtle_recursion.py
    #	Notes/NotesB/10_plotting.py
    #	Notes/NotesB/10_plotting_csv.py
    #	Problems/Sorting/05_sorting_problems.py

--- Labs/CTA Ridership/cta_rider_lab.py
+++ Labs/CTA Ridership/cta_rider_lab.py
@@ -1,9 +1,4 @@
-import matplotlib.pyplot as plt
-import csv
 
-with open("") as f:
-    reader = csv.reader(f)
-    data = list(reader)
 '''
 CTA Ridership (25pts)
 
@@ -18,9 +13,11 @@ It has been updated with 2018 data, but not yet with 2019 unfortunately
 3  Plot total usage on a third line on your graph. (5pts)
 4  Add a title and label your axes. (4pts)
 5  Add a legend to show data represented by each of the three lines. (4pts)
-6  What trend or trends do you see in the data?  Offer a hypotheses which might explain the trend(s). Just add a comment here to explain. (2pts)
+6  What trend or trends do you see in the data?  Offer a hypotheses which might explain the trend(s).
+Just add a comment here to explain. (2pts)
 '''
 
+import matplotlib.pyplot as plt
 import csv
 
 with open("CTA_-_Ridership_-_Annual_Boarding_Totals (1).csv") as f:
@@ -28,3 +25,63 @@ with open("CTA_-_Ridership_-_Annual_Boarding_Totals (1).csv") as f:
     data = list(cr)
 
 print(data)
+
+# headers
+headers = data.pop(0)
+
+# get last 10 years (years)
+years = [x[0] for x in data]
+ten_years = years[-10:]
+print(ten_years)
+
+# get last ten years bus
+bus = [int(x[1]) for x in data]
+ten_bus = bus[-10:]
+print(ten_bus)
+
+# get last ten years rail
+rail = [int(x[3]) for x in data]
+ten_rail = rail[-10:]
+print(ten_rail)
+
+# get last ten years total
+total = [int(x[-1]) for x in data]
+ten_total = total[-10:]
+print(ten_total)
+
+
+# plot bus
+plt.plot(ten_years, ten_bus, linewidth=2, label='Bus Usage', marker='o')
+
+# plot rail
+plt.plot(ten_years, ten_rail, linewidth=2, label='Rail Usage', marker='o')
+
+# plot total
+plt.plot(ten_years, ten_total, linewidth=2, label='Total Usage', marker='o')
+
+
+# axis labels
+plt.xlabel("Year", fontsize=15)
+plt.ylabel("Ridership (Hundred Millions)", fontsize=15)
+
+# title
+plt.title("CTA Ridership (2009 - 2018)", fontsize=20)
+
+# legend
+plt.legend()
+
+# show plot
+plt.show()
+
+
+# hypothesis / trend analysis (you said what trend/trends so I just analyzed one trend)
+
+'''
+After 2015, the total ridership progressively declined, possibly due to the increasing popularity of 
+ride-hailing services such as Uber and Lyft. Commute time is an important factor to many people, and using ride-hailing 
+services is often faster than public transportation, a factor contributing to their popularity. CTA ridership 
+(especially bus) is influenced by gas prices at any given time. Another possible reason for ridership declining (mostly 
+in the total and bus areas) could be low gas prices. This would also explain why rail ridership was not declining as
+severely, as it does not rely on gas. 
+'''
+
