5c7a8f042ed3ef7ece188d2fc3c36c731c84e8e9
isabelle <ibiehler@fwparker.org>
2020-03-25 13:31:20 -0500

    Merge branch 'master' of https://www.github.com/fwparkercode/P2_SP20
    # Conflicts:
    #	Notes/NotesB/08_recursion.py
    #	Notes/NotesB/09_turtle_recursion.py
    #	Notes/NotesB/10_plotting.py
    #	Notes/NotesB/10_plotting_csv.py
    #	Problems/Sorting/05_sorting_problems.py

--- Notes/NotesB/10_plotting_scatter.py
+++ Notes/NotesB/10_plotting_scatter.py
@@ -6,4 +6,32 @@ with open("World firearms murders and ownership - Sheet 1.tsv") as f:
     reader = csv.reader(f, delimiter='\t')
     data = list(reader)
 
-print(data)
+header = data.pop(0)
+print(header)
+
+# make a scatter of firearms_per_100  vs homicides_100k
+
+homicide_100k = []
+firearms_100 = []
+countries = []
+
+for country in data:
+    try:
+        homicides = float(country(5))
+        firearms = float(country(-2))
+        name = country[0]
+        homicide_100k.append(homicides)
+        firearms_100.append(firearms)
+        countries.append(name)
+    except:
+        print(country[0], "data is inadequate.")
+
+print(countries)
+
+plt.figure("Homicides per Firearm Worldwide")  # add figsize
+
+plt.scatter(firearms_100, homicide_100k)
+plt.ylabel("homicides per 100k population")
+plt.xlabel("firearms per 100 people")
+
+plt.show()
