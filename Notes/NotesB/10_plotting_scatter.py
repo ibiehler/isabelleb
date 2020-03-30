a27713a051384c359230919f0ce992978e53030f
isabelle <ibiehler@fwparker.org>
2020-03-29 20:51:30 -0500

    Merge branch 'master' of https://www.github.com/fwparkercode/P2_SP20
    # Conflicts:
    #	Notes/NotesB/10_plotting_scatter.py

--- Notes/NotesB/10_plotting_scatter.py
+++ Notes/NotesB/10_plotting_scatter.py
@@ -14,11 +14,16 @@ print(header)
 homicide_100k = []
 firearms_100 = []
 countries = []
+similar = ["Canada", "England and Wales", "Japan", "South Korea", "France", "Germany", "Switzerland",
+           "Ireland", "China", "Singapore", "Hungary", 'Israel', "Taiwan", "Iceland", "Spain", "Belgium",
+           "Finland", "Netherlands", "United States"]
+
+data = [x for x in data if x[0] in similar]
 
 for country in data:
     try:
-        homicides = float(country(5))
-        firearms = float(country(-2))
+        homicides = float(country[5])
+        firearms = float(country[-2])
         name = country[0]
         homicide_100k.append(homicides)
         firearms_100.append(firearms)
@@ -26,12 +31,19 @@ for country in data:
     except:
         print(country[0], "data is inadequate.")
 
+
 print(countries)
 
-plt.figure("Homicides per Firearm Worldwide")  # add figsize
+plt.figure("Homicides per Firearm Worldwide", figsize=(10, 6))  # add figsize
 
 plt.scatter(firearms_100, homicide_100k)
 plt.ylabel("homicides per 100k population")
 plt.xlabel("firearms per 100 people")
+plt.title("Homicides vs. Gun Ownership by Country")
+
+plt.annotate("MY TEXT", xy=(50, 50))
+
+for i in range(len(countries)):
+    plt.annotate(countries[i], xy=(firearms_100[i], homicide_100k[i]))
 
 plt.show()
