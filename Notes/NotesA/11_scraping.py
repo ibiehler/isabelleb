e0804a7b044ea810f19a299d8635d53a51256a1c
isabelle <ibiehler@fwparker.org>
2020-04-13 21:53:58 -0500

    Merge branch 'master' of https://www.github.com/fwparkercode/P2_SP20
    # Conflicts:
    #	Notes/NotesA/10_plotting_url.py
    #	Notes/NotesB/10_plotting_url.py

--- Notes/NotesA/11_scraping.py
+++ Notes/NotesA/11_scraping.py
@@ -6,16 +6,18 @@ import requests
 url = "http://quotes.toscrape.com/"  # uniform resource locator
 
 page = requests.get(url)
-print(page)  # 200 is a good request
+print(page)  # prints the response; 200 is a good request
+
 # print(page.text)  # text of the webpage (not that useful)
 
 
 soup = BeautifulSoup(page.text, "html.parser")
-print(soup.prettify())  # make it print formatted
+print(soup.prettify())  # make it print formatted; nicer way to look at it
 
-# find data in our soup object
-# by tagname
+# find data in our soup object by tagname
+# find method just gest the first one it finds
 title = soup.find("title")  # look for the <title> and returns the first one
 
 print(title)
+print(title.text)
 
